7ad8f9d6f6554388046b8e672fcf36cf29eec93f
Onfusia_ne_ona <144990841+AnfisaPavlukhina@users.noreply.github.com>
2024-09-13 16:38:38 +0300

    Create task.cpp

---
+++ task.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "function.hpp"
+int main()
+{
+        std::cout << "Main function" << std::endl;
+        std::cout << "output " << function(1, 2) << std::endl;
+        return 0;
+}
