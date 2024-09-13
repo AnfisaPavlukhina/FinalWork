254e3d5556873684c0cc20ed4e5fd258c1b37785
Onfusia_ne_ona <144990841+AnfisaPavlukhina@users.noreply.github.com>
2024-09-13 16:51:36 +0300

    Added tests

--- tests/test_all.cpp
+++ tests/test_all.cpp
@@ -3,3 +3,11 @@
 #include <stdexcept>
 
 #include "function.cpp"
+TEST (Function, Normal) {
+  EXPECT_EQ(8, function(2,2));
+  EXPECT_EQ(4, function(2,1));
+}
+TEST (Function, SecondTest) {
+  EXPECT_EQ(6, function(1,2));
+  EXPECT_EQ(3, function(1,1));
+}
